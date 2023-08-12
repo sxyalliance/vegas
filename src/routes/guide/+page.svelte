677cf7982221b403a3d2840594dfead59524a6e1
Sunxyw <git@sunxyw.xyz>
2023-08-12 22:58:55 +0800

    scaffold guide page

---
+++ src/routes/guide/+page.svelte
@@ -0,0 +1,15 @@
+<div class="py-32">
+    <div class="mx-auto max-w-7xl px-6 text-center lg:px-8">
+        <ul role="list"
+            class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <li>
+                <img class="mx-auto h-56 w-56 rounded-lg"
+                     src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                     alt="">
+                <h3 class="mt-6 text-xl font-semibold leading-7 tracking-tight text-gray-900">Rules</h3>
+            </li>
+
+            <!-- More people... -->
+        </ul>
+    </div>
+</div>
