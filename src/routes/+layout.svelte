ab3d6b8d3343e3c7ff86ca0b01c7f0e715f3f5c5
Sunxyw <git@sunxyw.xyz>
2023-09-18 17:59:11 +0800

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
+
   import Header from "$lib/components/layout/Header.svelte";
   import Footer from "$lib/components/layout/Footer.svelte";
 </script>
