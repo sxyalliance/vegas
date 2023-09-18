ccc88fb0eccee59068687c5e67497d9b361d80ce
Sunxyw <git@sunxyw.xyz>
2023-09-18 19:03:52 +0800

    add web vitals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
   import Header from "$lib/components/layout/Header.svelte";
   import Footer from "$lib/components/layout/Footer.svelte";
+  import {browser} from "$app/environment";
+  import {webVitals} from "$lib/vitals";
+  import {page} from "$app/stores";
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId
+    })
+  }
 </script>
 
 <Header/>
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,8 @@ export default defineConfig({
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  define: {
+    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
   }
 });
