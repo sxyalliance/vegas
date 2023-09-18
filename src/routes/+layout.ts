af9f60764e085331e259ca1a53d5f3ab1dcac434
Sunxyw <git@sunxyw.xyz>
2023-09-18 18:47:23 +0800

    change web analysitcs position

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,10 @@ import '$lib/i18n' // Import to initialize. Important :)
 import {locale, waitLocale} from 'svelte-i18n'
 import {resolveFirstAvailableLocale} from "$lib/i18n";
 
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+inject({ mode: dev ? 'development' : 'production' });
+
 export const load = async () => {
   if (browser) {
     locale.set(resolveFirstAvailableLocale(window.navigator.languages))
