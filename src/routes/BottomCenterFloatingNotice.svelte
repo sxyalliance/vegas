a04d1af634d9f58f1e65e3a345aeec4ed19c91dc
Sunxyw <git@sunxyw.xyz>
2023-09-19 20:44:26 +0800

    add develop notice back

--- src/routes/BottomCenterFloatingNotice.svelte
+++ src/routes/BottomCenterFloatingNotice.svelte
@@ -1,6 +1,6 @@
 <div class="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
-    <div class="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-900 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
-        <p class="text-sm leading-6 text-white">
+    <div class="pointer-events-auto flex items-center justify-between gap-x-6 bg-hue12 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+        <p class="text-sm leading-6 text-hue-fg">
             This site is still under development. Please report any bugs or issues to our press department via the
             community Discord.
         </p>
