833cb6e46190839e72d4af919bb8b21eba8b0bea
Sunxyw <git@sunxyw.xyz>
2023-09-29 00:32:39 +0800

    add loading for initial page load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,48 @@
 <script lang="ts">
-	import Header from '$lib/components/layout/Header.svelte';
-	import Footer from '$lib/components/layout/Footer.svelte';
-	import { browser } from '$app/environment';
-	import { navigating, page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
-	import BottomCenterFloatingNotice from './BottomCenterFloatingNotice.svelte';
-	import { title } from '$lib/stores/title';
+  import Header from "$lib/components/layout/Header.svelte";
+  import Footer from "$lib/components/layout/Footer.svelte";
+  import { browser } from "$app/environment";
+  import { navigating, page } from "$app/stores";
+  import { webVitals } from "$lib/vitals";
+  import BottomCenterFloatingNotice from "./BottomCenterFloatingNotice.svelte";
+  import { title } from "$lib/stores/title";
 
-	import './rainbow.css';
-	import LoadingOverlay from '$lib/components/LoadingOverlay.svelte';
-	import { isLoading } from 'svelte-i18n';
+  import "./rainbow.css";
+  import LoadingOverlay from "$lib/components/LoadingOverlay.svelte";
+  import { isLoading } from "svelte-i18n";
+  import { onMount } from "svelte";
 
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
 
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId
+    });
+  }
 
-	$: loading = $navigating || $isLoading;
+  // for initial page load, where navigating store is not available
+  let pageLoaded = false;
+  onMount(() => {
+    pageLoaded = true;
+  });
+
+  $: loading = $navigating || $isLoading || !pageLoaded;
 </script>
 
 <svelte:head>
-	<title>{$title}</title>
+  <title>{$title}</title>
 </svelte:head>
 
 {#if loading}
-	<LoadingOverlay />
+  <LoadingOverlay />
 {:else}
-	<Header />
+  <Header />
 
-	<slot />
+  <slot />
 
-	<Footer />
+  <Footer />
 {/if}
 
 <BottomCenterFloatingNotice />
