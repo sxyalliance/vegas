c8369bd4c6759a22c95400064d12ee5c7b00a3bf
Sunxyw <git@sunxyw.xyz>
2023-10-03 01:03:13 +0800

    add week calendar to gaming calendar

--- src/routes/calendar/gaming/+page.svelte
+++ src/routes/calendar/gaming/+page.svelte
@@ -1,113 +1,123 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-  import {
-    Availability,
-    getAvailabilityOfTime,
-    getDayOfWeek,
-    getStatementStatsOfTime,
-    getTimeOfDay,
-    TimeOfDay
-  } from "$lib/calendar/gaming";
+	import type { PageData } from './$types';
+	import {
+		Availability,
+		getAvailabilityOfTime,
+		getDayOfWeek,
+		getStatementStatsOfTime,
+		getTimeOfDay,
+		TimeOfDay
+	} from '$lib/calendar/gaming';
+	import SideBySideCalendar from './SideBySideCalendar.svelte';
 
-  export let data: PageData;
+	export let data: PageData;
 
-  const date = new Date();
-  const dayOfWeek = getDayOfWeek(date);
-  const timeOfDay = getTimeOfDay(date);
+	const date = new Date();
+	const dayOfWeek = getDayOfWeek(date);
+	const timeOfDay = getTimeOfDay(date);
 
-  const availability = timeOfDay
-    ? getAvailabilityOfTime(data.gamingCalendar, dayOfWeek, timeOfDay)
-    : Availability.NOT_FREE;
-  const statementStats = timeOfDay
-    ? getStatementStatsOfTime(data.gamingCalendar, dayOfWeek, timeOfDay)
-    : {};
+	const availability = timeOfDay
+		? getAvailabilityOfTime(data.gamingCalendar, dayOfWeek, timeOfDay)
+		: Availability.NOT_FREE;
+	const statementStats = timeOfDay
+		? getStatementStatsOfTime(data.gamingCalendar, dayOfWeek, timeOfDay)
+		: {};
 
-  const timeOfDayLong = {
-    [TimeOfDay.MORNING]: "Morning",
-    [TimeOfDay.AFTERNOON]: "Afternoon",
-    [TimeOfDay.EVENING]: "Evening",
-    [TimeOfDay.AFTER_DARK]: "After Dark",
-    null: "Break"
-  }[timeOfDay];
+	const timeOfDayLong = {
+		[TimeOfDay.MORNING]: 'Morning',
+		[TimeOfDay.AFTERNOON]: 'Afternoon',
+		[TimeOfDay.EVENING]: 'Evening',
+		[TimeOfDay.AFTER_DARK]: 'After Dark',
+		null: 'Break'
+	}[timeOfDay];
 
-  const availabilityLong = {
-    [Availability.AVAILABLE]: "Available",
-    [Availability.TENTATIVE]: "Tentative",
-    [Availability.NOT_FREE]: "Not Free"
-  }[availability];
+	const availabilityLong = {
+		[Availability.AVAILABLE]: 'Available',
+		[Availability.TENTATIVE]: 'Tentative',
+		[Availability.NOT_FREE]: 'Not Free'
+	}[availability];
 </script>
 
 <div class="bg-hue1 pt-36 pb-16 sm:pb-24">
-  <div class="mx-auto max-w-7xl px-6 lg:px-8">
-    <div class="mx-auto max-w-2xl lg:max-w-none">
-      <div class="text-center">
-        <h2 class="text-3xl font-bold tracking-tight text-hue12 sm:text-4xl">Gaming Time</h2>
-        <p class="mt-4 text-lg leading-8 text-hue11 max-w-3xl mx-auto">
-          This is a visual representation of the
-          <a
-            class="underline"
-            target="_blank"
-            href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsOKB8runaSLRM6UpWCzYNHknFPW96TvPHbhwUjEXsvJQkY3urBF3CH_7VM6bweczxsVVrBKzPjTUG/pubhtml">SXYA
-            Weekly Availability Statement</a>, which helps gaming members better manage their own schedule by allowing
-          them to verify the current gaming availability and potential play times this week.
-        </p>
-      </div>
-      <dl
-        class="mt-12 grid grid-cols-1 gap-0.5 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-4"
-      >
-        <div class="flex flex-col p-8">
-          <dt class="text-sm font-semibold leading-6 text-hue11">Day of Week</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-hue12">{dayOfWeek}</dd>
-        </div>
-        <div class="flex flex-col p-8">
-          <dt class="text-sm font-semibold leading-6 text-hue11">Time of Day</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-hue12">
-            {timeOfDayLong}
-          </dd>
-        </div>
-        <div class="flex flex-col p-8">
-          <dt class="text-sm font-semibold leading-6 text-hue11">Overall Availability</dt>
-          <dd
-            class="order-first text-3xl font-semibold tracking-tight text-hue12"
-            class:text-success10={availability === Availability.AVAILABLE}
-            class:text-warning10={availability === Availability.TENTATIVE}
-            class:text-error10={availability === Availability.NOT_FREE}
-          >
-            {timeOfDay ? availability + ' ' + availabilityLong : 'N/A'}
-          </dd>
-        </div>
-        <div class="flex flex-col p-8">
-          <dt class="text-sm font-semibold leading-6 text-hue11">Statements</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-hue12">
-            {#if timeOfDay}
-              <span class="text-success10">{statementStats[Availability.AVAILABLE]}</span>
-              |
-              <span class="text-warning10">{statementStats[Availability.TENTATIVE]}</span>
-              |
-              <span class="text-error10">{statementStats[Availability.NOT_FREE]}</span>
-            {:else}
-              N/A
-            {/if}
-          </dd>
-        </div>
-      </dl>
-    </div>
-  </div>
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl lg:max-w-none mb-6">
+			<div class="text-center">
+				<h2 class="text-3xl font-bold tracking-tight text-hue12 sm:text-4xl">Gaming Time</h2>
+				<p class="mt-4 text-lg leading-8 text-hue11 max-w-3xl mx-auto">
+					This is a visual representation of the
+					<a
+						class="underline"
+						target="_blank"
+						href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsOKB8runaSLRM6UpWCzYNHknFPW96TvPHbhwUjEXsvJQkY3urBF3CH_7VM6bweczxsVVrBKzPjTUG/pubhtml"
+						>SXYA Weekly Availability Statement</a
+					>, which helps gaming members better manage their own schedule by allowing them to verify
+					the current gaming availability and potential play times this week.
+				</p>
+			</div>
+			<dl
+				class="mt-12 grid grid-cols-1 gap-0.5 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-4"
+			>
+				<div class="flex flex-col p-8">
+					<dt class="text-sm font-semibold leading-6 text-hue11">Day of Week</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight text-hue12">{dayOfWeek}</dd>
+				</div>
+				<div class="flex flex-col p-8">
+					<dt class="text-sm font-semibold leading-6 text-hue11">Time of Day</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight text-hue12">
+						{timeOfDayLong}
+					</dd>
+				</div>
+				<div class="flex flex-col p-8">
+					<dt class="text-sm font-semibold leading-6 text-hue11">Overall Availability</dt>
+					<dd
+						class="order-first text-3xl font-semibold tracking-tight text-hue12"
+						class:text-success10={availability === Availability.AVAILABLE}
+						class:text-warning10={availability === Availability.TENTATIVE}
+						class:text-error10={availability === Availability.NOT_FREE}
+					>
+						{timeOfDay ? availability + ' ' + availabilityLong : 'N/A'}
+					</dd>
+				</div>
+				<div class="flex flex-col p-8">
+					<dt class="text-sm font-semibold leading-6 text-hue11">Statements</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight text-hue12">
+						{#if timeOfDay}
+							<span class="text-success10">{statementStats[Availability.AVAILABLE]}</span>
+							|
+							<span class="text-warning10">{statementStats[Availability.TENTATIVE]}</span>
+							|
+							<span class="text-error10">{statementStats[Availability.NOT_FREE]}</span>
+						{:else}
+							N/A
+						{/if}
+					</dd>
+				</div>
+			</dl>
+		</div>
+
+		<div
+			class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"
+		>
+			<div class="rounded-md shadow-2xl ring-1 ring-gray-900/10 p-6">
+				<SideBySideCalendar calendar={data.gamingCalendar} />
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="bg-hue2">
-  <div class="px-6 py-12 sm:px-6 sm:py-16 lg:px-8">
-    <div class="mx-auto max-w-2xl text-center">
-      <h2 class="text-3xl font-bold tracking-tight text-hue12 sm:text-4xl">
-        Don't want to miss any games?<br />
-        Subscribe to gaming notifications.
-      </h2>
-      <p class="mx-auto mt-2 max-w-xl text-lg leading-8 text-hue11">
-        Be notified when we are about to start playing.
-      </p>
-      <div class="mt-4 flex items-center justify-center">
-        <a href="javascript:" class="btn-xl btn-primary">Coming Soon</a>
-      </div>
-    </div>
-  </div>
+	<div class="px-6 py-12 sm:px-6 sm:py-16 lg:px-8">
+		<div class="mx-auto max-w-2xl text-center">
+			<h2 class="text-3xl font-bold tracking-tight text-hue12 sm:text-4xl">
+				Don't want to miss any games?<br />
+				Subscribe to gaming notifications.
+			</h2>
+			<p class="mx-auto mt-2 max-w-xl text-lg leading-8 text-hue11">
+				Be notified when we are about to start playing.
+			</p>
+			<div class="mt-4 flex items-center justify-center">
+				<a href="javascript:" class="btn-xl btn-primary">Coming Soon</a>
+			</div>
+		</div>
+	</div>
 </div>
---
+++ src/routes/calendar/gaming/SideBySideCalendar.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import Icon from '@iconify/svelte';
+	import type { GamingCalendar } from '$lib/calendar/gaming';
+	import {
+		Availability,
+		getAvailabilityOfTime,
+		getDayOfWeek,
+		TimeOfDay
+	} from '$lib/calendar/gaming';
+
+	export let calendar: GamingCalendar;
+
+	let selected = new Date();
+	const today = dayjs();
+	$: days = getDaysForCalendarMonth(today).map((date) => {
+		const day = dayjs(date);
+		return {
+			date: day.toDate(),
+			isToday: day.isSame(today, 'date'),
+			isCurrentPeriod: day.isSame(today, 'week'),
+			isSelected: day.isSame(selected, 'date')
+		};
+	});
+
+	function getDaysForCalendarMonth(date) {
+		const firstDayOfMonth = dayjs(date).startOf('month');
+		const firstDayOfCal = firstDayOfMonth.clone().startOf('week');
+		const lastDayOfMonth = firstDayOfMonth.clone().endOf('month');
+		const lastDayOfCal = lastDayOfMonth.clone().endOf('week');
+		let temp = firstDayOfCal.clone();
+		const days = [temp.toDate()];
+		while (temp.isBefore(lastDayOfCal) && days.length < 42) {
+			temp = temp.add(1, 'day');
+			days.push(temp.toDate());
+		}
+		while (days.length < 42) {
+			temp = temp.add(1, 'day');
+			days.push(temp.toDate());
+		}
+		return days;
+	}
+
+	const availabilityLongMap = {
+		[Availability.AVAILABLE]: 'Available',
+		[Availability.TENTATIVE]: 'Tentative',
+		[Availability.NOT_FREE]: 'Not Free'
+	};
+
+	function getDetailsOfDay(date: Date) {
+		const day = getDayOfWeek(date);
+
+		return [
+			{
+				icon: 'meteocons:time-late-morning',
+				period: 'Morning',
+				availability: getAvailabilityOfTime(calendar, day, TimeOfDay.MORNING)
+			},
+			{
+				icon: 'meteocons:time-afternoon',
+				period: 'Afternoon',
+				availability: getAvailabilityOfTime(calendar, day, TimeOfDay.AFTERNOON)
+			},
+			{
+				icon: 'meteocons:time-evening',
+				period: 'Evening',
+				availability: getAvailabilityOfTime(calendar, day, TimeOfDay.EVENING)
+			},
+			{
+				icon: 'meteocons:time-late-evening',
+				period: 'After Dark',
+				availability: getAvailabilityOfTime(calendar, day, TimeOfDay.AFTER_DARK)
+			}
+		];
+	}
+
+	$: details = getDetailsOfDay(selected);
+</script>
+
+<div class="md:grid md:grid-cols-2 md:divide-x md:divide-hue6">
+	<div class="md:pr-14">
+		<div class="flex items-center">
+			<h2 class="flex-auto text-sm font-semibold text-hue12">January 2022</h2>
+		</div>
+		<div class="mt-10 grid grid-cols-7 text-center text-xs leading-6 text-hue10">
+			<div>S</div>
+			<div>M</div>
+			<div>T</div>
+			<div>W</div>
+			<div>T</div>
+			<div>F</div>
+			<div>S</div>
+		</div>
+		<div class="mt-2 grid grid-cols-7 text-sm">
+			{#each days as day, dayIdx (day.date)}
+				<div class="py-2 {dayIdx > 6 ? 'border-t border-hue4' : ''}">
+					<button
+						type="button"
+						on:click={() => (selected = day.date)}
+						disabled={!day.isCurrentPeriod}
+						class:text-white={day.isSelected}
+						class:text-primary10={!day.isSelected && day.isToday}
+						class:text-hue12={!day.isSelected && !day.isToday && day.isCurrentPeriod}
+						class:text-hue6={!day.isSelected && !day.isToday && !day.isCurrentPeriod}
+						class:bg-primary10={day.isSelected && day.isToday}
+						class:bg-hue12={day.isSelected && !day.isToday}
+						class:hover:bg-hue3={!day.isSelected}
+						class:font-semibold={day.isSelected || day.isToday}
+						class="mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+					>
+						<time datetime={day.date}>{day.date.getDate()}</time>
+					</button>
+				</div>
+			{/each}
+		</div>
+	</div>
+	<section class="mt-12 md:mt-0 md:pl-14">
+		<h2 class="text-base font-semibold leading-6 text-hue12">
+			Availability for
+			<time datetime={selected}>{selected.toLocaleDateString()}</time>
+		</h2>
+		<ol class="mt-4 space-y-1 text-sm leading-6 text-hue10">
+			{#each details as detail}
+				<li
+					class="group flex items-center space-x-4 rounded-xl p-2 focus-within:bg-hue2 hover:bg-hue1"
+				>
+					<Icon icon={detail.icon} class="h-15 w-15 flex-none -m-2 -mt-4" />
+					<div class="flex-auto">
+						<p class="text-hue12">{detail.period}</p>
+						<p class="mt-0.5">
+							{detail.availability}
+							{availabilityLongMap[detail.availability]}
+						</p>
+					</div>
+				</li>
+			{/each}
+		</ol>
+	</section>
+</div>
