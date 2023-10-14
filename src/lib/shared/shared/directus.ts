89fe3bc3cb2444bfe1c67e23f1dc2173b4eda072
Sunxyw <git@sunxyw.xyz>
2023-10-15 04:50:10 +0800

    refactor: bump typescript to v5

--- src/lib/shared/shared/directus.ts
+++ src/lib/shared/shared/directus.ts
@@ -1,8 +1,7 @@
-import type { DirectusClient, RestClient } from '@directus/sdk';
 import type { DirectusSchema } from '../../../../directus';
 import { createDirectus, rest } from '@directus/sdk';
 
-export type ConstructedDirectusClient = DirectusClient<DirectusSchema> & RestClient<DirectusSchema>;
+export type ConstructedDirectusClient = ReturnType<typeof constructDirectus>;
 
 export const constructDirectus = (customFetch: typeof window.fetch) => {
 	return createDirectus<DirectusSchema>('http://apid.sxya.org:18055', {
--- src/routes/api/events/+server.ts
+++ src/routes/api/events/+server.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from './$types';
 import { readItems } from '@directus/sdk';
-import type { DirectusSchema } from "../../../../directus";
 
 export const GET: RequestHandler = async ({ locals: { directus } }) => {
 	const events = await directus.request(readItems('events'));
