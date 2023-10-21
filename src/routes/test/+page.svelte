562f9d30d4e9e86d47ce746d7800cfe62eaae121
Sunxyw <git@sunxyw.xyz>
2023-10-21 21:32:48 +0800

    refactor: prepare i18n migrate

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -3,9 +3,7 @@
 	import * as m from '@inlang/paraglide-js/vegas/messages';
 </script>
 
-<button on:click={() => setLanguageTag('de')}> change the language to "de" </button>
-<button on:click={() => setLanguageTag('en')}>change the language to "en" </button>
-<button on:click={() => setLanguageTag('zh-HK')}>change the language to "zh-HK" </button>
+<button on:click={() => setLanguageTag('en-US')}>change the language to "en"</button>
+<button on:click={() => setLanguageTag('zh-HK')}>change the language to "zh-HK"</button>
 
-<h1>{m.hello()}</h1>
-<p>{@html m.docs()}</p>
+<h1>{m.brand_name()}</h1>
