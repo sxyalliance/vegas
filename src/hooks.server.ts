6a1850db32b922f4168b6281fd2e88a144e1369d
Sunxyw <git@sunxyw.xyz>
2023-10-24 00:09:39 +0800

    fix: disable Sentry since not working on Cloudflare Page

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,27 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import * as Sentry from '@sentry/sveltekit';
+// import * as Sentry from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import { constructDirectus } from '$lib/shared/directus/client';
 import { localePreference, resolveFirstAvailableLocale } from '$lib/shared/i18n';
 
-Sentry.init({
-	dsn: 'https://d0d7d2be65f2a949deeacc88600dca80@o4506100443119616.ingest.sentry.io/4506100444692480',
-	tracesSampleRate: 1
-});
+// Blocked by: https://github.com/getsentry/sentry-javascript/issues/8291
+// Sentry.init({
+// 	dsn: 'https://d0d7d2be65f2a949deeacc88600dca80@o4506100443119616.ingest.sentry.io/4506100444692480',
+// 	tracesSampleRate: 1
+// });
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
-	const langs = event.request.headers.get('accept-language')?.split(',');
-	if (langs) {
-		localePreference.set(resolveFirstAvailableLocale(langs));
+export const handle: Handle = sequence(
+	/*Sentry.sentryHandle(),*/ async ({ event, resolve }) => {
+		const langs = event.request.headers.get('accept-language')?.split(',');
+		if (langs) {
+			localePreference.set(resolveFirstAvailableLocale(langs));
+		}
+
+		event.locals.directus = constructDirectus(event.fetch);
+
+		return resolve(event, {
+			filterSerializedResponseHeaders: (name) => !name.startsWith('x-')
+		});
 	}
-
-	event.locals.directus = constructDirectus(event.fetch);
-
-	return resolve(event, {
-		filterSerializedResponseHeaders: (name) => !name.startsWith('x-')
-	});
-});
-export const handleError = Sentry.handleErrorWithSentry();
+);
+// export const handleError = Sentry.handleErrorWithSentry();
