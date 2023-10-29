af3ff4e5b191b1bfe520769437b3889a310f91f4
Sunxyw <git@sunxyw.xyz>
2023-10-30 01:09:58 +0800

    fix: animate.css not included

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -1,19 +1,19 @@
 import type { RO_Sitemap } from 'sveltekit-sitemap';
 
 export const sitemap = (<const>{
-	'/': true,
-	'/about/faq': false,
-	'/about/members': false,
-	'/about': true,
-	'/api': true,
-	'/calendar/gaming': false,
-	'/calendar': true,
-	'/events': true,
-	'/events/[slug]': false,
-	'/lab/ai': false,
-	'/lab': true,
-	'/policies/privacy': false,
-	'/policies': true
-}) satisfies RO_Sitemap;
+   "/": true,
+   "/about/faq": false,
+   "/about/members": false,
+   "/about": true,
+   "/api": true,
+   "/calendar/gaming": false,
+   "/calendar": true,
+   "/events": true,
+   "/events/[slug]": false,
+   "/lab/ai": false,
+   "/lab": true,
+   "/policies/privacy": false,
+   "/policies": true
+}) satisfies RO_Sitemap
 
-export type Sitemap = typeof sitemap;
+export type Sitemap = typeof sitemap
