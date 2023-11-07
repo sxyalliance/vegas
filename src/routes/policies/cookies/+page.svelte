4bca5ebf39a468455634f8ba5afa7d3cbd273c5d
Eric-ouo <a812574@gmail.com>
2023-11-07 19:53:40 +0800

    build:cookies
    This reverts commit 806924fc96dec74ffec5539d654574fe2ba090e6.

---
+++ src/routes/policies/cookies/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import SeoHandler from '$lib/shared/seo/SeoHandler.svelte';
+
+	import Content from './cookie_content.md';
+</script>
+
+<SeoHandler
+	metaTags={{
+		title: 'Cookie Policy'
+	}}
+/>
+
+<section class="bg-neutral-1 px-6 py-32 lg:px-8">
+	<article class="prose-here prose mx-auto max-w-3xl">
+		<Content />
+	</article>
+</section>
