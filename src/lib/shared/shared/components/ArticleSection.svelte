8555e63a90ec08f8c9d21b50714e70a7da6d2099
Sunxyw <git@sunxyw.xyz>
2023-11-08 19:24:44 +0800

    refactor: abstract article section

---
+++ src/lib/shared/shared/components/ArticleSection.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Section from '$lib/vgui/section/Section.svelte';
+
+	let clazz: string | false | null = null;
+	export { clazz as class };
+
+	/**
+	 * Uses an alternate background color.
+	 */
+	export let alternate: boolean = false;
+</script>
+
+<Section {alternate} class={clazz}>
+	<article class="prose-here prose mx-auto">
+		<slot />
+	</article>
+</Section>
--- src/routes/policies/privacy/+page.svelte
+++ src/routes/policies/privacy/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import SeoHandler from '$lib/shared/seo/SeoHandler.svelte';
 
+	import ArticleSection from '$lib/shared/shared/components/ArticleSection.svelte';
+
 	import { html } from './content.md';
 </script>
 
@@ -10,9 +12,7 @@
 	}}
 />
 
-<section class="bg-neutral-1 px-6 py-32 lg:px-8">
-	<article class="prose-here prose mx-auto max-w-3xl">
-		<!-- eslint-disable-next-line svelte/no-at-html-tags -- content from local file -->
-		{@html html}
-	</article>
-</section>
+<ArticleSection>
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -- content from local file -->
+	{@html html}
+</ArticleSection>
