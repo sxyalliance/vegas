5d8be685c9c7dcb3471bed55d3a568b661dc7cf3
Sunxyw <git@sunxyw.xyz>
2023-11-15 21:12:10 +0800

    feat: add preferences sheet to replace theme switch

--- src/lib/shared/theme/ThemeSwitch.svelte
+++ src/lib/shared/theme/ThemeSwitch.svelte
@@ -1,61 +1,113 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-
-	import Icon from '@iconify/svelte';
-
 	import * as m from '$lib/shared/i18n/compiled/messages';
-	import { themePreference, theme } from '$lib/shared/theme/store';
+	import { availableThemeModes, availableThemes } from '$lib/shared/theme/options';
+	import { dayTheme, nightTheme, Theme, ThemeMode, themeMode } from '$lib/shared/theme/preferences';
+	import { Label } from '$lib/vgui/components/ui/label';
+	import * as Select from '$lib/vgui/components/ui/select';
 
-	import { Button } from '$lib/vgui/components/ui/button';
-	import * as DropdownMenu from '$lib/vgui/components/ui/dropdown-menu';
+	import type { SelectOption } from '@melt-ui/svelte';
 
-	let clazz = '';
-	export { clazz as class };
+	$: currentModeOption =
+		availableThemeModes.find((v) => v.value === $themeMode) || availableThemeModes[0];
+	$: dayThemeOption = availableThemes.find((v) => v.value === $dayTheme) || availableThemes[0];
+	$: nightThemeOption = availableThemes.find((v) => v.value === $nightTheme) || availableThemes[-1];
 
-	theme.subscribe((value: string) => {
-		if (!browser) {
-			return;
+	const handleModeSelectedChange = (v: SelectOption<unknown> | undefined) => {
+		if (v) {
+			themeMode.set(v.value as ThemeMode);
 		}
+	};
 
-		document.documentElement.dataset.theme = value;
-	});
-
-	const availableThemePreferences = [
-		{
-			name: 'system',
-			label: m.theme_switch_system(),
-			icon: 'lucide:contrast'
-		},
-		{
-			name: 'light',
-			label: m.theme_switch_light(),
-			icon: 'lucide:sun'
-		},
-		{
-			name: 'dark',
-			label: m.theme_switch_dark(),
-			icon: 'lucide:moon'
+	const handleDayThemeSelectedChange = (v: SelectOption<unknown> | undefined) => {
+		if (v) {
+			dayTheme.set(v.value as Theme);
 		}
-	];
+	};
+
+	const handleNightThemeSelectedChange = (v: SelectOption<unknown> | undefined) => {
+		if (v) {
+			nightTheme.set(v.value as Theme);
+		}
+	};
 </script>
 
-<DropdownMenu.Root positioning={{ placement: 'bottom-end' }}>
-	<DropdownMenu.Trigger asChild let:builder>
-		<Button builders={[builder]} variant="outline" size="icon" class={clazz}>
-			{#each availableThemePreferences as themePreferenceC (themePreferenceC.icon)}
-				{#if themePreferenceC.name === $theme}
-					<Icon icon={themePreferenceC.icon} class="h-5 w-5" />
-				{/if}
-			{/each}
-			<span class="sr-only">{m.theme_switch_label()}</span>
-		</Button>
-	</DropdownMenu.Trigger>
-	<DropdownMenu.Content>
-		{#each availableThemePreferences as themePreferenceC (themePreferenceC.name)}
-			<DropdownMenu.Item on:click={() => themePreference.set(themePreferenceC.name)}>
-				<Icon icon={themePreferenceC.icon} class="mr-3 h-4 w-4" />
-				{themePreferenceC.label}
-			</DropdownMenu.Item>
-		{/each}
-	</DropdownMenu.Content>
-</DropdownMenu.Root>
+<div class="space-y-4">
+	<div class="space-y-2">
+		<Label>{m.theme_mode_label()}</Label>
+		<Select.Root
+			portal={null}
+			onSelectedChange={handleModeSelectedChange}
+			selected={{ value: currentModeOption.value, label: m[currentModeOption.label]() }}
+		>
+			<Select.Trigger>
+				<Select.Value asChild>
+					<div>
+						{m[currentModeOption.label]()}
+						<span class="ml-2 text-muted-foreground">
+							{m[currentModeOption.description]()}
+						</span>
+					</div>
+				</Select.Value>
+			</Select.Trigger>
+			<Select.Content>
+				{#each availableThemeModes as mode (mode.value)}
+					<Select.Item value={mode.value} label={m[mode.label]()} class="flex justify-between">
+						{m[mode.label]()}
+						<span class="text-muted-foreground">
+							{m[mode.description]()}
+						</span>
+					</Select.Item>
+				{/each}
+			</Select.Content>
+			<Select.Input name="theme_mode" />
+		</Select.Root>
+	</div>
+
+	<div class="grid grid-cols-1 gap-x-4" class:md:grid-cols-2={$themeMode === 'auto'}>
+		<div class="space-y-2">
+			{#if $themeMode === 'auto'}
+				<Label>{m.theme_day_theme()}</Label>
+			{/if}
+			<Select.Root
+				portal={null}
+				onSelectedChange={handleDayThemeSelectedChange}
+				selected={{ value: dayThemeOption.value, label: m[dayThemeOption.label]() }}
+			>
+				<Select.Trigger>
+					<Select.Value />
+				</Select.Trigger>
+				<Select.Content>
+					{#each availableThemes as theme (theme.value)}
+						<Select.Item value={theme.value} label={m[theme.label]()}
+							>{m[theme.label]()}</Select.Item
+						>
+					{/each}
+				</Select.Content>
+				<Select.Input name="day_theme" />
+			</Select.Root>
+		</div>
+
+		{#if $themeMode === 'auto'}
+			<div class="space-y-2">
+				<Label>{m.theme_night_theme()}</Label>
+				<Select.Root
+					portal={null}
+					onSelectedChange={handleNightThemeSelectedChange}
+					selected={{ value: nightThemeOption.value, label: m[nightThemeOption.label]() }}
+				>
+					<Select.Trigger>
+						<Select.Value />
+					</Select.Trigger>
+					<Select.Content>
+						{#each availableThemes as theme (theme.value)}
+							<Select.Item value={theme.value} label={m[theme.label]()}
+								>{m[theme.label]()}</Select.Item
+							>
+						{/each}
+					</Select.Content>
+					<Select.Input name="night_theme" />
+				</Select.Root>
+			</div>
+		{/if}
+	</div>
+</div>
